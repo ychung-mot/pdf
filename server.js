ea604e0434d3a19d483bcd1309c576631d10f46f
Young-Jin Chung <44654482+ychung-mot@users.noreply.github.com>
2019-03-28 14:38:42 -0700

    removed unused package

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var express = require('express'),
     pdf     = require('./pdf'),
     moment  = require('moment');
 
-Object.assign=require('object-assign')
-
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
